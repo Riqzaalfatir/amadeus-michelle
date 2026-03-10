da383ae41f54945518837dc682694b3571421685
Maulana Riqza Al-Fatir <riqzapersonal@gmail.com>
2026-03-10 19:43:51 +0700

    testing layout

--- src/components/invitation/MobileContainer.tsx
+++ src/components/invitation/MobileContainer.tsx
@@ -14,27 +14,27 @@ const MobileContainer = () => {
         data-aos-delay="50"
         className="stripe absolute top-0 left-0 right-0" />
 
-        <div className="pt-[30px] md:pt-[100px] lg:pt-[80px] pb-[110px] md:pb-[100px] lg:pb-[120px] flex flex-col items-center">
+        <div className="pt-[50px] md:pt-[100px] lg:pt-[80px] pb-[120px] md:pb-[100px] lg:pb-[120px] flex flex-col items-center">
 
           <div className="text-center">
             <h1 
             data-aos="fade-down"
             data-aos-delay="150"
-            className="font-nephilm text-[48px] tracking-wide text-[#2E497C]">
+            className="font-nephilm text-[40px] md:text-[48px] tracking-wide text-[#2E497C]">
               AMADEUS
             </h1>
 
             <p 
             data-aos="fade-up"
             data-aos-delay="300"
-            className="text-[#E0A786] font-breadley text-[31px]">
+            className="text-[#E0A786] font-breadley text-[26px] md:text-[31px]">
               and
             </p>
 
             <h1 
             data-aos="fade-down"
             data-aos-delay="450"
-            className="font-nephilm text-[48px] tracking-wide text-[#2E497C]">
+            className="font-nephilm text-[40px] md:text-[48px] tracking-wide text-[#2E497C]">
               MICHELLE
             </h1>
           </div>
@@ -46,14 +46,14 @@ const MobileContainer = () => {
             alt="Hero"
             width={300}
             height={300}
-            className="object-contain w-[260px] md:w-[420px] lg:w-[340px]"
+            className="object-contain w-[220px] md:w-[420px] lg:w-[340px]"
             priority
           />
 
           <div 
           data-aos="fade-right"
           data-aos-delay="750"
-          className="relative z-10 mb-[5px] md:mb-[5px] font-breadley text-[21px] leading-[100%] tracking-[3px] text-center text-[#c79a73]">
+          className="relative z-10 mt-[10px] font-breadley text-[18px] md:text-[21px] tracking-[3px] text-center text-[#c79a73]">
             24.01.26
           </div>
 
@@ -65,7 +65,7 @@ const MobileContainer = () => {
         data-aos-delay="900"
         data-aos-anchor-placement="top-bottom"
         className="stripe absolute bottom-0 left-0 right-0 flex items-center justify-center">
-          <span className="text-[#2E497C] px-4 py-1.5 text-[12px] tracking-[2px] font-breadley">
+          <span className="text-[#2E497C] px-4 py-1.5 text-[11px] md:text-[12px] tracking-[2px] font-breadley">
             #AureliaFernandoWedding
           </span>
         </div>
