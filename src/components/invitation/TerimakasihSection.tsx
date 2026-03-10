2b7b44615ff80a379ef90e10a7132d2251af54d7
Maulana Riqza Al-Fatir <riqzapersonal@gmail.com>
2026-03-11 04:25:52 +0700

    animasi v2

--- src/components/invitation/TerimakasihSection.tsx
+++ src/components/invitation/TerimakasihSection.tsx
@@ -20,22 +20,22 @@ const TerimakasihSection = () => {
       <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row items-start md:justify-between">
 
         {/* TEXT */}
-        <div className="w-full md:w-[510px] -mr-0 md:-mr-[180px] z-20 pt-[70px]">
+        <div className="w-full md:w-[510px] -mr-0 md:-mr-[140px] z-20 md:pt-[60px] translate-y-[94px] md:translate-y-0">
           <h2 className="text-[#2E497C] text-[28px] font-nephilm uppercase tracking-[1px]">
             Terima Kasih
           </h2>
 
-          <p className="text-[#C75376] text-[14px] font-breadley mt-4 leading-relaxed tracking-[1px] max-w-[224px]">
+          <p className="text-[#C75376] text-[14px] font-breadley mt-1 leading-relaxed tracking-[px] md:tracking-[1px] max-w-[220px] md:max-w-[224px]">
             Semoga Anda berkenan untuk hadir dan menikmati rangkaian acara
             pada hari bahagia kami.
           </p>
         </div>
 
         {/* FOTO & FRAME */}
-        <div className="relative w-full md:w-[370px] h-[340px] mt-8 md:mt-0 flex justify-center items-center">
+        <div className="relative w-[480px]  md:w-[370px] h-[210px] md:h-[340px] mt-0 flex justify-center items-center">
 
           {/* FRAME */}
-          <div className="absolute w-[220px] h-[300px] md:w-[300px] md:h-[380px]">
+          <div className="absolute w-[260px] h-[320px] md:w-[300px] md:h-[380px]">
             <Image
               src={Frame}
               alt="frame"
@@ -45,7 +45,7 @@ const TerimakasihSection = () => {
           </div>
 
           {/* FOTO */}
-          <div className="absolute top-[80px] md:top-[98px] right-[0px] md:-right-[7px] w-[150px] h-[150px] md:w-[190px] md:h-[190px] overflow-hidden rounded-lg">
+          <div className="absolute top-[32px] md:top-[78px] right-[138px] md:-right-[8px] w-[150px] h-[150px] md:w-[170px] md:h-[190px]  overflow-hidden rounded-lg">
             <Image
               src={Couple}
               alt="couple"
