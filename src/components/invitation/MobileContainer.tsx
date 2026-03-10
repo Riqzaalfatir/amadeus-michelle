c7e309d6a0e52d447cbbc8d24302ff3a0b8ba43e
Maulana Riqza Al-Fatir <riqzapersonal@gmail.com>
2026-03-10 20:04:00 +0700

    testing layout

--- src/components/invitation/MobileContainer.tsx
+++ src/components/invitation/MobileContainer.tsx
@@ -3,15 +3,16 @@ import HeroKanan from "@/assets/HeroKanan.png";
 
 const MobileContainer = () => {
   return (
-    <div className="w-full min-h-screen flex justify-center bg-[#F6EEE1]">
+    <div className="w-full h-screen flex justify-center bg-[#F6EEE1]">
 
-      <div className="relative w-full min-h-screen flex flex-col items-center
+      <div className="w-full h-screen flex flex-col items-center
       max-w-[420px] md:max-w-[520px] lg:max-w-[620px]">
 
         {/* Stripe Atas */}
         <div className="stripe w-full" />
 
-        <div className="pt-[80px] md:pt-[100px] lg:pt-[80px] pb-[80px] md:pb-[100px] lg:pb-[120px] flex flex-col items-center">
+        {/* Konten Tengah */}
+        <div className="flex-1 flex flex-col items-center justify-center">
 
           <div className="text-center">
             <h1 className="font-nephilm text-[48px] tracking-wide text-[#2E497C]">
@@ -36,14 +37,14 @@ const MobileContainer = () => {
             priority
           />
 
-          <div className="mt-[12px] md:mt-[16px] font-breadley text-[21px] leading-[100%] tracking-[3px] text-center text-[#c79a73]">
+          <div className="mt-[12px] font-breadley text-[21px] tracking-[3px] text-[#c79a73]">
             24.01.26
           </div>
 
         </div>
 
         {/* Stripe Bawah */}
-        <div className="stripe mt-auto w-full flex items-center justify-center">
+        <div className="stripe w-full flex items-center justify-center">
           <span className="text-[#2E497C] px-4 py-1.5 text-[12px] tracking-[2px] font-breadley">
             #AureliaFernandoWedding
           </span>
