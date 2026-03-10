5050d7d530e653409119c1336e975d886744fe76
Maulana Riqza Al-Fatir <riqzapersonal@gmail.com>
2026-03-10 19:57:43 +0700

    testing layout

--- src/components/invitation/MobileContainer.tsx
+++ src/components/invitation/MobileContainer.tsx
@@ -9,39 +9,25 @@ const MobileContainer = () => {
       max-w-[420px] md:max-w-[520px] lg:max-w-[620px]">
 
         {/* Stripe Atas */}
-        <div 
-        data-aos="fade-down"
-        data-aos-delay="50"
-        className="stripe absolute top-0 left-0 right-0" />
+        <div className="stripe w-full" />
 
         <div className="pt-[80px] md:pt-[100px] lg:pt-[80px] pb-[80px] md:pb-[100px] lg:pb-[120px] flex flex-col items-center">
 
           <div className="text-center">
-            <h1 
-            data-aos="fade-down"
-            data-aos-delay="150"
-            className="font-nephilm text-[48px] tracking-wide text-[#2E497C]">
+            <h1 className="font-nephilm text-[48px] tracking-wide text-[#2E497C]">
               AMADEUS
             </h1>
 
-            <p 
-            data-aos="fade-up"
-            data-aos-delay="300"
-            className="text-[#E0A786] font-breadley text-[31px]">
+            <p className="text-[#E0A786] font-breadley text-[31px]">
               and
             </p>
 
-            <h1 
-            data-aos="fade-down"
-            data-aos-delay="450"
-            className="font-nephilm text-[48px] tracking-wide text-[#2E497C]">
+            <h1 className="font-nephilm text-[48px] tracking-wide text-[#2E497C]">
               MICHELLE
             </h1>
           </div>
 
           <Image
-            data-aos="zoom-in"
-            data-aos-delay="600"
             src={HeroKanan}
             alt="Hero"
             width={300}
@@ -50,21 +36,14 @@ const MobileContainer = () => {
             priority
           />
 
-          <div 
-          data-aos="fade-up"
-          data-aos-delay="750"
-          className="mt-[12px] md:mt-[16px] font-breadley text-[21px] leading-[100%] tracking-[3px] text-center text-[#c79a73]">
+          <div className="mt-[12px] md:mt-[16px] font-breadley text-[21px] leading-[100%] tracking-[3px] text-center text-[#c79a73]">
             24.01.26
           </div>
 
         </div>
 
         {/* Stripe Bawah */}
-        <div 
-        data-aos="fade-up"
-        data-aos-delay="900"
-        data-aos-anchor-placement="top-bottom"
-        className="stripe absolute bottom-0 left-0 right-0 flex items-center justify-center">
+        <div className="stripe mt-auto w-full flex items-center justify-center">
           <span className="text-[#2E497C] px-4 py-1.5 text-[12px] tracking-[2px] font-breadley">
             #AureliaFernandoWedding
           </span>
