40a6249b1b81134cfa1153bf59db420fef14c027
Maulana Riqza Al-Fatir <riqzapersonal@gmail.com>
2026-03-10 19:40:13 +0700

    testing layout

--- src/components/invitation/MobileContainer.tsx
+++ src/components/invitation/MobileContainer.tsx
@@ -3,9 +3,9 @@ import HeroKanan from "@/assets/HeroKanan.png";
 
 const MobileContainer = () => {
   return (
-    <div className="w-full h-screen flex justify-center bg-[#F6EEE1]">
+    <div className="w-full min-h-screen flex justify-center bg-[#F6EEE1]">
 
-      <div className="relative w-full h-screen flex flex-col items-center
+      <div className="relative w-full min-h-screen flex flex-col items-center
       max-w-[420px] md:max-w-[520px] lg:max-w-[620px]">
 
         {/* Stripe Atas */}
